aef34ff12d20faf2b5b1c5bd9b31c5eb3dec655e
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-08-17 13:30:20 -0400

    add iterable interface

---
+++ Integral.Interface/Abstractions/Iterable.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Integral.Abstractions
+{
+    public interface Iterable
+    {
+        void Iterate();
+    }
+
+    public interface Iterable<Return>
+        where Return : Task
+    {
+        Return Iterate(CancellationToken cancellationToken);
+    }
+}
