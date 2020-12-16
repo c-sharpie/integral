a24e2ddc0cda2606e9cf7dbd33f1b36fba74b8d1
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-12-16 14:07:42 -0500

    update version number

--- Integral.Interface/Aggregates/PairedDecreasingAggregate.cs
+++ Integral.Interface/Aggregates/PairedDecreasingAggregate.cs
@@ -3,11 +3,13 @@
     public interface PairedDecreasingAggregate
     {
         bool Remove<Key, Element>(Key key, out Element element)
-            where Key : notnull;
+            where Key : notnull
+            where Element : notnull;
     }
 
     public interface PairedDecreasingAggregate<in Key, Element>
         where Key : notnull
+        where Element : notnull
     {
         bool Remove(Key key, out Element element);
     }
